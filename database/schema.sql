48c0f6cb7f4bbabb61add8e30ff84dfd00fa55bf
davidfsobek <davidfsobek@gmail.com>
2018-10-28 18:54:29 -0700

    updated schema

--- database/schema.sql
+++ database/schema.sql
@@ -21,3 +21,27 @@ name text,
 price real,
 location jsonb
 );
+
+CREATE TABLE users_test (
+user_id uuid DEFAULT uuid_generate_v4 (),
+password text not null,
+access_key text not null,
+email text not null,
+phone_number int not null,
+full_name int not null,
+preferences jsonb
+);
+
+CREATE TABLE flex_info_test (
+user_id uuid not null,
+meal_plan text,
+current_flex real
+);
+
+CREATE TABLE product_info_test (
+product_id uuid DEFAULT uuid_generate_v4 (),
+barcode int,
+name text,
+price real,
+location jsonb
+);
