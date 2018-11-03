8f548f36f871d83b0b65791b77ae4d5a295bf078
josh711 <jcabral@rtlogic.com>
2018-11-03 14:12:13 -0700

    Have debug and Secret key read from env variables

--- flex_tracker/settings.py
+++ flex_tracker/settings.py
@@ -23,10 +23,19 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '***REMOVED***'
-
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+#DEBUG = True
+try:
+    DEBUG = os.environ['DJANGO_DEBUG']
+except:
+    DEBUG = 'True'
+
+if DEBUG == 'False':
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = []
 
