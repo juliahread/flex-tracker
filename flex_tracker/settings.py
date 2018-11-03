b09116c960f8ff4ecab9f8ae2294e311cfb66321
josh711 <jcabral@rtlogic.com>
2018-11-03 14:13:22 -0700

    Add error catching for reading from env variables

--- flex_tracker/settings.py
+++ flex_tracker/settings.py
@@ -23,7 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+try:
+    SECRET_KEY = os.environ['SECRET_KEY']
+except:
+    SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 
 #DEBUG = True
