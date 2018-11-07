e51e13b52afba035bf7d1fffb0689b266789d9ff
Julia Read <juliahread@gmail.com>
2018-11-06 20:27:31 -0800

    Merging with hamburger stuff

--- flex_backend/views.py
+++ flex_backend/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Context, loader
 
 def index(request):
-    flex= u'\U0001F4AA'
-    return HttpResponse("Hello, world. You're at the " + str(flex) + " index!")
+    #flex= u'\U0001F4AA'
+    #return HttpResponse("Hello, world. You're at the " + str(flex) + " index!")
+    #template = loader.get_template("templates/login.html")
+    #context = {}
+    return render(request, "settings.html")
+    #return render(request, '/templates/login.html', {})
--- flex_tracker/settings.py
+++ flex_tracker/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'flex_tracker',
+    'flex_backend',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +66,7 @@ ROOT_URLCONF = 'flex_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
