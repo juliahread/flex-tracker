97a3903dc09584b12cb44ad748e5f224c298b785
yemilyz <ezhao@g.hmc.edu>
2018-12-12 00:12:05 -0800

    change redirection to home from accounts

--- accounts/views.py
+++ accounts/views.py
@@ -11,16 +11,12 @@ def SignUp(request):
             user.refresh_from_db()
             user.phone_number = form.cleaned_data.get("phone_number")
             user.provider = form.cleaned_data.get("provider")
-            user.save()            
+            user.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-<<<<<<< HEAD
-            return redirect('tutorial')
-=======
             return redirect('home')
->>>>>>> 434556aa6e8e1680914b7230694a20440d93ef9a
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form, 'error': 'test'})
